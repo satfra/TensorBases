25a9943f91de304e5ee865257a0eb5ed106778e5
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2025-03-11 22:03:32 +0100

    feat: Better pure glue bases: Classical and transverse classical four- and three-gluon bases.
    Signed-off-by: Franz R. Sattler <sattler@thphys.uni-heidelberg.de>

--- bases/AAAAClass.m
+++ bases/AAAAClass.m
@@ -24,41 +24,42 @@ ff[ca_,cb_,cc_,cd_]:=Module[{i},TBF[color,i,ca,cb]TBF[color,i,cc,cd]]
 
 
 (* ::Input::Initialization:: *)
-TBAutoDefine["AAAA2"]=True;
+TBAutoDefine["AAAAClass"]=True;
 
-TBRequiredGroups["AAAA2"]={{color,SUNfund,Nc}};
+TBRequiredGroups["AAAAClass"]={{color,SUNfund,Nc}};
 
-TBVertex["AAAA2"]="AAAA2";
+TBVertex["AAAAClass"]="AAAAClass";
 
-TBVertexBasis["AAAA2"]=True;
+TBVertexBasis["AAAAClass"]=True;
 
-TBComment["AAAA2"]="Four-gluon vertex basis";
+TBComment["AAAAClass"]="Four-gluon vertex basis";
 
-TBAuthor["AAAA2"]="FR Sattler";
+TBAuthor["AAAAClass"]="FR Sattler";
 
-TBUsage["AAAA2"]={"Indices: 
+TBUsage["AAAAClass"]={"Indices: 
 	A: {\!\(\*SubscriptBox[\(p\), \(1\)]\),\[Mu],\!\(\*SubscriptBox[\(a\), \(1\)]\)}
 	A: {\!\(\*SubscriptBox[\(p\), \(2\)]\),\[Nu],\!\(\*SubscriptBox[\(a\), \(2\)]\)}
 	A: {\!\(\*SubscriptBox[\(p\), \(3\)]\),\[Rho],\!\(\*SubscriptBox[\(a\), \(3\)]\)}
 	A: {\!\(\*SubscriptBox[\(p\), \(4\)]\),\[Sigma],\!\(\*SubscriptBox[\(a\), \(4\)]\)}
+This vertex basis contains only the classical tensor structure.
 We use the general form 
-	\[ScriptCapitalL] = (2\[Pi]\!\(\*SuperscriptBox[\()\), \(d\)]\)\[Delta](\!\(\*SubscriptBox[\(p\), \(1\)]\)+\!\(\*SubscriptBox[\(p\), \(2\)]\)+\!\(\*SubscriptBox[\(p\), \(3\)]\)+\!\(\*SubscriptBox[\(p\), \(4\)]\))\!\(\*SubscriptBox[\(\[Tau]\), \(i\)]\)
+	V = (2\[Pi]\!\(\*SuperscriptBox[\()\), \(d\)]\)\[Delta](\!\(\*SubscriptBox[\(p\), \(1\)]\)+\!\(\*SubscriptBox[\(p\), \(2\)]\)+\!\(\*SubscriptBox[\(p\), \(3\)]\)+\!\(\*SubscriptBox[\(p\), \(4\)]\))\!\(\*SubscriptBox[\(\[Tau]\), \(i\)]\)
 and the \!\(\*SubscriptBox[\(\[Tau]\), \(i\)]\) are listed in the following:    ",
 TableForm[
 {
-{"\!\(\*FractionBox[\(1\), \(8\)]\)\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(ca\), \(1\)] \*SubscriptBox[\(a\), \(2\)]\)]\)\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(ca\), \(3\)] \*SubscriptBox[\(a\), \(4\)]\)]\)(\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Rho]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Sigma]\)]\)-\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Sigma]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Rho]\)]\))"}
+{"\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(1\)] \*SubscriptBox[\(a\), \(2\)]\)]\)\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(3\)] \*SubscriptBox[\(a\), \(4\)]\)]\)(\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Rho]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Sigma]\)]\)-\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Sigma]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Rho]\)]\))+\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(1\)] \*SubscriptBox[\(a\), \(3\)]\)]\)\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(2\)] \*SubscriptBox[\(a\), \(4\)]\)]\)(\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Nu]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Rho]\[Sigma]\)]\)-\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Sigma]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Rho]\)]\))+\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(1\)] \*SubscriptBox[\(a\), \(4\)]\)]\)\!\(\*SuperscriptBox[\(f\), \(\*SubscriptBox[\(a\), \(2\)] \*SubscriptBox[\(a\), \(3\)]\)]\)(\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Nu]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Rho]\[Sigma]\)]\)-\!\(\*SubscriptBox[\(\[Delta]\), \(\[Mu]\[Rho]\)]\)\!\(\*SubscriptBox[\(\[Delta]\), \(\[Nu]\[Sigma]\)]\))"}
 },TableHeadings->{{1}, {"Tensor"}},TableSpacing->{3, 3}]
 };
 
-TBIndices["AAAA2"]={{p1,v1,a1},{p2,v2,a2},{p3,v3,a3},{p4,v4,a4}};
+TBIndices["AAAAClass"]={{p1,v1,a1},{p2,v2,a2},{p3,v3,a3},{p4,v4,a4}};
 
-TBMomentumConservation["AAAA2"]={(*p4->-p1-p2-p3*)};
+TBMomentumConservation["AAAAClass"]={(*p4->-p1-p2-p3*)};
 
-TBBasis["AAAA2"]={
+TBBasis["AAAAClass"]={
 (ff[a1,a2,a3,a4](TBdeltaLorentz[v1,v3]TBdeltaLorentz[v2,v4]-TBdeltaLorentz[v1,v4]TBdeltaLorentz[v2,v3])
 +ff[a1,a3,a2,a4](TBdeltaLorentz[v1,v2]TBdeltaLorentz[v3,v4]-TBdeltaLorentz[v1,v4]TBdeltaLorentz[v2,v3])
 +ff[a1,a4,a2,a3](TBdeltaLorentz[v1,v2]TBdeltaLorentz[v3,v4]-TBdeltaLorentz[v1,v3]TBdeltaLorentz[v2,v4]))
 	(*T^(1)*)
 };
 
-TBCanonicalProduct["AAAA2"]=Tensor1[1,2,3,4]Tensor2[1,2,3,4];
+TBCanonicalProduct["AAAAClass"]=Tensor1[1,2,3,4]Tensor2[1,2,3,4];
