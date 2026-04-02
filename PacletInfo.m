ad774558a1c50c6fb60d515060c5aac05a33eb0c
Franz R. Sattler <fsattler@physik.uni-bielefeld.de>
2026-04-02 11:46:34 +0200

    Update PacletInfo.m

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,3 +1,3 @@
 (* ::Package:: *)
 
-PacletObject[<|"Name" -> "TensorBases", "Version" -> "1.1.5", "WolframVersion" -> "12.0+", "Extensions" -> {{"Kernel", "Context" -> {{"TensorBases`", "TensorBases.m"}}}, {"Documentation", "Language" -> "English"}}|>]
+PacletObject[<|"Name" -> "TensorBases", "Version" -> "1.1.6", "WolframVersion" -> "11.0+", "Extensions" -> {{"Kernel", "Context" -> {{"TensorBases`", "TensorBases.m"}}}, {"Documentation", "Language" -> "English"}}|>]
